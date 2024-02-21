37473a9f3adfc63562c51ecd931b0b05068c735a
elisabeth ospina <elisabethospinag@gmail.com>
2024-02-21 14:14:28 -0500

    crear componente password y reintalar nodemodules

--- Ejercicios/Elisabeth/FrontEnd/frontEnd/src/app/components/dashboard/cambiar-password/cambiar-password.component.ts
+++ Ejercicios/Elisabeth/FrontEnd/frontEnd/src/app/components/dashboard/cambiar-password/cambiar-password.component.ts
@@ -1,12 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cambiar-password',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterLink],
   templateUrl: './cambiar-password.component.html',
   styleUrl: './cambiar-password.component.css'
 })
 export class CambiarPasswordComponent {
+ cambiarPassword: FormGroup;
 
+ constructor(private fb: FormBuilder){
+  this.cambiarPassword = this.fb.group({
+    passwordAnterior: ['', Validators.required],
+    nuevaPassword: ['', [Validators.required, Validators.minLength(4)]],
+    confirmPassword: ['']
+  }, {validator: this.checkPassword})
+ }
+
+ checkPassword(group: FormGroup): any {
+  const pass = group.controls['password'].value;
+  const confirmPass = group.controls['confirmPassword'].value;
+  return pass === confirmPass ? null : { notSame: true};
+}
+
+  guardarPassword(){
+    console.log(this.cambiarPassword);
+
+  }
 }
--- Ejercicios/Elisabeth/FrontEnd/frontEnd/src/app/components/inicio/login/login.component.ts
+++ Ejercicios/Elisabeth/FrontEnd/frontEnd/src/app/components/inicio/login/login.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { Usuario } from '../../../models/usuario';
 import { LoadingComponent } from '../../../shared/loading/loading.component';
@@ -9,42 +9,45 @@ import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink,
-            CommonModule,
-            FormsModule,
-            ReactiveFormsModule,
-            LoadingComponent,
-            ],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterLink, LoadingComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  public login: FormGroup;
+  login: FormGroup;
+  loading:  boolean = false;
 
   constructor(private fb: FormBuilder,
     private toastr: ToastrService,
     private router: Router)
     {
-    this.login = this.fb.group({
+    this.login = fb.group({
       usuario: ['', Validators.required],
       password: ['', Validators.required]
     });
   }
   ngOnInit(): void {
   }
-  log(): void {
-    console.log(this.login);
 
+  log(): void {
     const usuario: Usuario = {
       nombreUsuario: this.login.value.usuario,
       password: this.login.value.password
     }
-    if (usuario.nombreUsuario === 'truizdiaz' && usuario.password === 'admin123') {
-      this.router.navigate(['/dashboard'])
-    } else{
-      // this.toastr.error('Error de autenticación','Acceso denegado');
-      this.toastr.error('usuario o contraseña incorrecto','Error');
-    }
+    this.loading = true;
+    setTimeout(() => {
+      this.loading = false;
+      if (usuario.nombreUsuario === 'elisabeth' && usuario.password === 'admin123')
+      {
+        this.login.reset();
+        this.router.navigate(['/dashboard'])
+      } else{
+        this.toastr.error('usuario o contraseña incorrecto', 'Error');
+        this.login.reset();
+      }
+      this.loading= false;
+    } , 2000)
+
     console.log(usuario);
 
   }
