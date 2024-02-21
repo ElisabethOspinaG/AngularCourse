5ae2a2b2d5bf144e28ce42da7f10c1283aaee865
elisabeth ospina <elisabethospinag@gmail.com>
2024-02-21 05:19:37 -0500

    creando el componente loading

--- Ejercicios/Elisabeth/FrontEnd/frontEnd/src/app/app.component.ts
+++ Ejercicios/Elisabeth/FrontEnd/frontEnd/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { LoadingComponent } from './shared/loading/loading.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, InicioComponent, ReactiveFormsModule],
+  imports: [CommonModule, RouterOutlet, InicioComponent, ReactiveFormsModule, LoadingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Ejercicios/Elisabeth/FrontEnd/frontEnd/src/app/components/inicio/login/login.component.ts
+++ Ejercicios/Elisabeth/FrontEnd/frontEnd/src/app/components/inicio/login/login.component.ts
@@ -1,20 +1,30 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { Usuario } from '../../../models/usuario';
+import { LoadingComponent } from '../../../shared/loading/loading.component';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink, CommonModule, FormsModule, ReactiveFormsModule ],
+  imports: [RouterLink,
+            CommonModule,
+            FormsModule,
+            ReactiveFormsModule,
+            LoadingComponent,
+            ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   public login: FormGroup;
 
-  constructor(private fb: FormBuilder){
+  constructor(private fb: FormBuilder,
+    private toastr: ToastrService,
+    private router: Router)
+    {
     this.login = this.fb.group({
       usuario: ['', Validators.required],
       password: ['', Validators.required]
@@ -28,6 +38,12 @@ export class LoginComponent {
     const usuario: Usuario = {
       nombreUsuario: this.login.value.usuario,
       password: this.login.value.password
+    }
+    if (usuario.nombreUsuario === 'truizdiaz' && usuario.password === 'admin123') {
+      this.router.navigate(['/dashboard'])
+    } else{
+      // this.toastr.error('Error de autenticación','Acceso denegado');
+      this.toastr.error('usuario o contraseña incorrecto','Error');
     }
     console.log(usuario);
 
