ea2ab2b316ca1401acaf23399ea0634782ff80ac
Pedro Ospina <Pedro.Ospina@coxautoinc.com>
2024-02-27 13:00:05 -0500

    Web api db connnection

---
+++ Ejemplos/Back/WebApiDatabaseConnection/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiDatabaseConnection.Services;
+
+namespace WebApiDatabaseConnection.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        LoginService service = new LoginService();
+        [HttpGet("Login")]
+        public bool Login(string username, string password)
+        {
+            return service.VerifyCredentials(username, password);
+        }
+
+        [HttpPost("Register")]
+        public bool Register(string name, string lastName, string username, string password)
+        {
+            return service.CreateAccount(name, lastName,username,password);
+        }
+    }
+}
